c346ad42b0a62c70c3fbd4d6474c6c0094e97efb
Gabriel Bastos <g43210@hotmail.com>
2026-01-08 22:13:14 +0000

    fix: remocao do layer button

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -21,11 +21,11 @@ MIRYOKU_X(FUN,    "Fun")
 #define U_EXTRA  1
 #define U_TAP    2
 //#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_NAV    3
+#define U_MOUSE  4
+#define U_MEDIA  5
+#define U_NUM    6
+#define U_SYM    7
+#define U_FUN    8
 
 #endif
